932b52e94c5dda3fc30fa250b79f238347ed3a62
John Wostenberg <jwosty@users.noreply.github.com>
2020-08-24 13:18:51 -0500

    downgrade jdk to 1.11

--- src/main/java/com/wostenberg/minecraft/playergreeter/PlayerGreeterPlugin.java
+++ src/main/java/com/wostenberg/minecraft/playergreeter/PlayerGreeterPlugin.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.*;
-import java.util.logging.Level;
 import java.util.regex.Pattern;
 
 import org.bukkit.Server;
